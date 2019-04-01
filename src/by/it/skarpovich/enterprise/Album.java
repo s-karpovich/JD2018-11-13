5ae12dedd6bba9af553eccf80cc08a253ee904ca
s-karpovich <mdcatalog2@gmail.com>
2019-04-01 03:29:11 +0300

    ee

---
+++ src/by/it/skarpovich/enterprise/Album.java
@@ -0,0 +1,30 @@
+package by.it.skarpovich.enterprise;
+
+import java.util.ArrayList;
+
+public class Album {
+
+    private String name;
+    private ArrayList<Track> tracks;
+
+    Album(String name, ArrayList<Track> tracks) {
+        this.name = name;
+        this.tracks = tracks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(ArrayList<Track> tracks) {
+        this.tracks = tracks;
+    }
+}
